b3e0137b4093138d03cd913db472728750f39034
Chris Tram <chris.tram@gmail.com>
2018-07-23 00:11:10 -0700

    remove useless nesting of divs

--- src/components/board-settings.js
+++ src/components/board-settings.js
@@ -26,65 +26,61 @@ export default class BoardSettings extends Component {
 
   render() {
     return (
-      <div>
-        <form className="form-inline" onSubmit={this.newBoard}>
-          <div className="form-group">
-            <label htmlFor="input-width" className="mr-3">
-              Width
-            </label>
-            <input
-              id="input-width"
-              className="form-control"
-              type="number"
-              ref="input-width"
-              min="1"
-              max="20"
-              placeholder="width"
-              value={this.state.width}
-              onChange={this.handeChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="input-height" className="mx-3">
-              Height
-            </label>
-            <input
-              id="input-height"
-              className="form-control"
-              type="number"
-              ref="input-height"
-              min="1"
-              max="20"
-              placeholder="height"
-              value={this.state.height}
-              onChange={this.handeChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="input-mines" className="mx-3">
-              Mines
-            </label>
-            <input
-              id="input-mines"
-              className="form-control"
-              type="number"
-              ref="input-mines"
-              min="1"
-              max="20"
-              placeholder="number of mines"
-              value={this.state.mines}
-              onChange={this.handeChange}
-            />
-          </div>
-          <button className="btn btn-primary ml-3" onClick={this.newBoard}>
-            Create Board
-          </button>
-          <small className="form-text text-muted ml-3">
-            Creating a new board will quit the current game!
-          </small>
-        </form>
-        <hr />
-      </div>
+      <form className="form-inline" onSubmit={this.newBoard}>
+        <div className="form-group">
+          <label htmlFor="input-width" className="mr-3">
+            Width
+          </label>
+          <input
+            id="input-width"
+            className="form-control"
+            type="number"
+            ref="input-width"
+            min="1"
+            max="20"
+            placeholder="width"
+            value={this.state.width}
+            onChange={this.handeChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="input-height" className="mx-3">
+            Height
+          </label>
+          <input
+            id="input-height"
+            className="form-control"
+            type="number"
+            ref="input-height"
+            min="1"
+            max="20"
+            placeholder="height"
+            value={this.state.height}
+            onChange={this.handeChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="input-mines" className="mx-3">
+            Mines
+          </label>
+          <input
+            id="input-mines"
+            className="form-control"
+            type="number"
+            ref="input-mines"
+            min="1"
+            placeholder="number of mines"
+            value={this.state.mines}
+            onChange={this.handeChange}
+          />
+        </div>
+        <button className="btn btn-primary ml-3" onClick={this.newBoard}>
+          Create Board
+        </button>
+        <small className="form-text text-muted ml-3">
+          Creating a new board will quit the current game!
+        </small>
+      </form>
     );
   }
 }
